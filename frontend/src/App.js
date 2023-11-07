b46b5f4847c3039e69ed81fbf9b531d5bd716f64
David <121127992+davidgit3000@users.noreply.github.com>
2023-11-07 15:15:20 -0800

    PR: Merge FE with BE (#5)
    * search bar + useState Hook implemented
    
    * add DescriptionBox.js
    
    * working on routing and creating two pages (article search results and artive view pages)
    
    ---------
    
    Co-authored-by: Patrick Nguyen <86446120+CerealAaa@users.noreply.github.com>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,40 +1,30 @@
 import Header from "./Components/Header.js";
-import DescriptionBox from "./Components/DescriptionBox.js";
 import { Stack } from "react-bootstrap";
+import DescriptionBox from "./Components/DescriptionBox.js";
 import logo from "./assets/logo.png";
-import ML_logo from "./assets/ml_brain.jpg";
+import { Route, Routes, Navigate } from "react-router-dom";
+import Home from "./pages/Home.js";
+import ArticleList from "./pages/ArticleList.js";
+import ArticleView from "./pages/ArticleView.js";
 
 function App() {
   return (
     <>
-      {/* <BrowserRouter>
-        <Routes>
-          <Route index element={<App />} />
-          <Route path="artice_list" element={<ArticleCard />} />
-          <Route path="article_view" element={<ArticleView />} />
-          <Route path="my_profile" element={<Profile />} />
-        </Routes>
-      </BrowserRouter> */}
-      <Stack gap={3}>
-        <div>
-          <Header />
-        </div>
-        <div className="position-absolute w-100 pb-2" style={{ top: 100 }}>
-          <DescriptionBox
-            title={"What is CS Catalog?"}
-            imgSrc={logo}
-            width={30}
-            height={15}
-          >
-            CS Catalog is a helpful site where we are seeking to faciliate
-            understanding and exposure to Machine Learning (ML), which has been
-            an incredibly popular discripline these days. We are aiming at
-            assisting those students who are uncertain where to start in ML. Our
-            target users will be Computer Science students who are passionate
-            about ML or are planning to get into this area.
-          </DescriptionBox>
-        </div>
-      </Stack>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/article_search_results">
+          <Route index element={<ArticleList />} />
+          <Route path=":id" element={<ArticleList />} />
+        </Route>
+        <Route path="/article_view">
+          <Route index element={<ArticleView />} />
+          <Route path=":name" element={<ArticleView />} />
+        </Route>
+        <Route
+          path="*"
+          element={<h1 className="text-center">404 - Page Not Found</h1>}
+        />
+      </Routes>
     </>
   );
 }
--- frontend/src/Components/SearchBar.js
+++ frontend/src/Components/SearchBar.js
@@ -1,51 +1,44 @@
 import { Card, Stack, Form } from "react-bootstrap";
 import { MDBCol } from "mdbreact";
-import DescriptionBox from "./DescriptionBox";
-// import { Search } from "react-bootstrap-icons";
-// import { useState } from "react";
+import { useState, useEffect } from "react";
+import ArticleList from "../pages/ArticleList";
+import { useParams } from "react-router-dom";
 
 export default function SearchBar() {
-  //const [description, setDescription] = useState("");
-  //description should be set to the text from the search bar
-  const onSubmitForm = async (e) => {
-    const description = document.getElementById("text_input");
-    console.log(description.value);
+  const [data, setData] = useState("");
+  // const { keyword } = useParams();
 
-    e.preventDefault();
-    try {
-      const response = await fetch(
-        `http://localhost:5000/api/articles/?title=${description.value}`,
-        { method: "GET" }
-      );
-      const jsonData = await response.json();
-      console.log(jsonData);
-      //call the next page here with an argument of jsonData
-      // for (let data of jsonData) {
-      //   <DescriptionBox></DescriptionBox>
-      // }
-    } catch (err) {
-      console.error(err.message);
-    }
+  useEffect(() => {
+    fetch("https://api.example.com/data") // modify the API link
+      .then((response) => response.json())
+      .then((data) => setData(data))
+      .catch((error) => console.error(error));
+  }, []);
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    alert(`The article you would like to search is: ${data}`);
   };
 
   return (
     <>
       <MDBCol md="9">
         <Card className="rounded-pill">
-          <Form onSubmit={onSubmitForm}>
+          <Form onSubmit={submitHandler}>
             <Stack direction="horizontal">
               <Form.Control
                 id="text_input"
                 className="rounded-pill border-0"
                 placeholder="Search articles here..."
                 style={{ boxShadow: "none" }}
+                value={data}
+                onChange={(e) => setData(e.target.value)}
               />
               <Form.Control
                 type="submit"
                 value={"Search"}
                 className="bg-primary w-auto rounded-pill"
               />
-              {/* <Search className="m-1"></Search> */}
             </Stack>
           </Form>
         </Card>
