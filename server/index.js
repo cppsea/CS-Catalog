6558fc44093dd1c48e2eef0f268f8f581c20d56b
plexin123 <67119125+plexin123@users.noreply.github.com>
2024-03-21 17:22:51 -0700

    Create ImageStorage
    add image Storage file

--- server/index.js
+++ server/index.js
@@ -2,8 +2,11 @@ const express = require("express");
 const articleRoutes = require("./src/articles/routes.js");
 const courseRoutes = require("./src/courses/routes.js");
 const userRoutes = require("./src/users/routes.js");
+const imagesRoutes = require("./src/images/routes.js");
 const cors = require("cors");
-require("dotenv").config();
+const dotenv = require("dotenv");
+dotenv.config();
+// require("dotenv").config();
 
 
 console.log(process.env.PGUSER);
@@ -12,9 +15,11 @@ console.log(process.env.PGUSER);
 
 
 const app = express();
+// const number = 8080
 
 const port = process.env.PORT;
 
+const port1= 8080
 
 app.use(cors());
 app.use(express.json());
@@ -25,6 +30,6 @@ app.get("/", (req, res) => {});
 app.use("/api/articles", articleRoutes);
 app.use("/api/courses", courseRoutes);
 app.use('/api/users', userRoutes);
+app.use("/api/images", imagesRoutes); // images routes
 
-
-app.listen(port, () => console.log(`app listening on port ${port}`));
+app.listen(port1, () => console.log(`app listening on port ${port1}`));
